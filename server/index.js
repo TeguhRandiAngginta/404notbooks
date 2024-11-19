11c62fcffea13795761896ec1a6416e73476761b
TeguhRandiAngginta <160731921+TeguhRandiAngginta@users.noreply.github.com>
2024-11-19 11:33:49 +0700

    menambahkan middleware pada configs

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import {db} from "./configs/db.js";
 import userRouter from "./routes/user.route.js";//import role router
+import { errorHandler } from "./configs/middleware.js";
 
 const app = express(); //Library express initialized
 const PORT = process.env.PORT; 
@@ -11,6 +12,17 @@ app.use(express.json());
 
 //api
 app.use("/api/v1/users", userRouter);
+
+// succes handling
+app.use('/', (req, res) => {
+    res.status(200).json({message: 'Hello World!'});
+});
+
+// error handling
+app.use('*', (req, res) => {
+    res.status(404).json({message: 'not found'});
+});
+app.use(errorHandler);
 
 
 // route / path
