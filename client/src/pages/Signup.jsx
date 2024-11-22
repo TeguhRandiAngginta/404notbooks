61c2bb49b0d6b158fc5a99a788c144f95301384e
aharist <aharist03@gmail.com>
2024-11-22 22:14:51 +0700

    mendesign sign up

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import logo from '../assets/logo/logoWeb.png';
+import img from '../assets/logo/log1.png';
 import { useForm } from 'react-hook-form';
 import './style.css';
 
@@ -20,97 +24,106 @@ export default function Signup() {
     };
 
     return (
-        <Card className="p-4">
-            <Row>
-                <Col>
-                    <h2>test</h2>
-                </Col>
-                <Col>
-                    <h1>Masuk Akun 404</h1>
-                    <Form onSubmit={handleSubmit(doSubmit)}>
-                        {/* Email Field */}
-                        <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
-                            <Form.Control
-                                type="email"
-                                placeholder="name@example.com"
-                                {...register('email', { required: 'Masukkan email' })}
-                            />
-                            {errors.email && (
-                                <Form.Text className="text-danger">{errors.email.message}</Form.Text>
-                            )}
-                        </FloatingLabel>
+        <Container fluid className="d-flex justify-content-center align-items-center vh-100 container">
+            <Card className="p-5 w-75">
+                <Row>
+                    {/* Bagian Kiri */}
+                    <Col md={6} className="text-center">
+                        <div className="d-flex justify-content-center align-items-center mb-3 mt-5">
+                            <Image src={logo} roundedCircle className="logo me-2" />
+                            <h2>Not Books</h2>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <Image src={img} rounded className="log1" />
+                        </div>
+                    </Col>
 
-                        {/* Username Field */}
-                        <FloatingLabel controlId="floatingInput" label="Nama Lengkap" className="mb-3">
-                            <Form.Control
-                                type="text"
-                                placeholder="Alexandro Dronolo"
-                                {...register('username', { required: 'Masukkan Nama Lengkap' })}
-                            />
-                            {errors.username && (
-                                <Form.Text className="text-danger">{errors.username.message}</Form.Text>
-                            )}
-                        </FloatingLabel>
+                    {/* Bagian Kanan */}
+                    <Col md={6}> 
+                        
+                        <Form onSubmit={handleSubmit(doSubmit)} className='center' >
+                        <h1 className="mb-4">Daftar Akun 404</h1>
+                            {/* Email Field */}
+                            <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+                                <Form.Control
+                                    type="email"
+                                    placeholder="name@example.com"
+                                    {...register('email', { required: 'Masukkan email' })}
+                                />
+                                {errors.email && (
+                                    <Form.Text className="text-danger">{errors.email.message}</Form.Text>
+                                )}
+                            </FloatingLabel>
 
-                        {/* Password Field */}
-                        <FloatingLabel controlId="floatingPassword" label="Kata Sandi">
-                            <Form.Control
-                                type="password"
-                                placeholder="Password"
+                            {/* Username Field */}
+                            <FloatingLabel controlId="floatingInput" label="Nama Lengkap" className="mb-3">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="Alexandro Dronolo"
+                                    {...register('username', { required: 'Masukkan Nama Lengkap' })}
+                                />
+                                {errors.username && (
+                                    <Form.Text className="text-danger">{errors.username.message}</Form.Text>
+                                )}
+                            </FloatingLabel>
+
+                            {/* Password Field */}
+                            <FloatingLabel controlId="floatingPassword" label="Kata Sandi">
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    className="mb-3"
+                                    {...register('password', { required: 'Masukkan Kata Sandi' })}
+                                />
+                                {errors.password && (
+                                    <Form.Text className="text-danger">{errors.password.message}</Form.Text>
+                                )}
+                            </FloatingLabel>
+
+                            {/* Confirm Password Field */}
+                            <FloatingLabel controlId="floatingConfirmPassword" label="Ulangi Kata Sandi">
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password"
+                                    className="mb-3"
+                                    {...register('confirmPassword', { required: 'Masukkan konfirmasi kata sandi' })}
+                                />
+                                {errors.confirmPassword && (
+                                    <Form.Text className="text-danger">
+                                        {errors.confirmPassword.message}
+                                    </Form.Text>
+                                )}
+                            </FloatingLabel>
+
+                            <Form.Check
+                                type="checkbox"
+                                id="agreementCheckbox"
+                                label={
+                                    <span>
+                                        Saya menyetujui <a href="#" className="link-dark"><b>kebijakan privasi</b></a><br />404 Not Books
+                                    </span>
+                                }
                                 className="mb-3"
-                                {...register('password', { required: 'Masukkan Kata Sandi' })}
+                                {...register('agreement', { required: 'Kamu harus menyetujui kebijakan privasi' })}
                             />
-                            {errors.password && (
-                                <Form.Text className="text-danger">{errors.password.message}</Form.Text>
+                            {errors.agreement && (
+                                <Form.Text className="text-danger">{errors.agreement.message}</Form.Text>
                             )}
-                        </FloatingLabel>
 
-                        {/* Confirm Password Field */}
-                        <FloatingLabel controlId="floatingConfirmPassword" label="Ulangi Kata Sandi">
-                            <Form.Control
-                                type="password"
-                                placeholder="Password"
-                                className="mb-3"
-                                {...register('confirmPassword', { required: 'Masukkan konfirmasi kata sandi' })}
-                            />
-                            {errors.confirmPassword && (
-                                <Form.Text className="text-danger">
-                                    {errors.confirmPassword.message}
-                                </Form.Text>
-                            )}
-                        </FloatingLabel>
-
-                        {/* Agreement Checkbox */}
-                        <Form.Check
-                            type="checkbox"
-                            id="agreementCheckbox"
-                            label={
-                                <span>
-                                    Saya menyetujui{' '}
-                                    <a href="#" className="link-dark">
-                                        <b>kebijakan privasi</b>
-                                    </a>{' '}
-                                    404 Not Books
-                                </span>
-                            }
-                            className="mb-3"
-                            {...register('agreement', { required: 'Kamu harus menyetujui kebijakan privasi' })}
-                        />
-                        {errors.agreement && (
-                            <Form.Text className="text-danger">{errors.agreement.message}</Form.Text>
-                        )}
-
-                        {/* Submit Button */}
-                        <Button
-                            type="submit"
-                            disabled={isSubmitting}
-                            className="btn btn-info mt-2 wide-button"
-                        >
-                            {isSubmitting ? 'Loading...' : 'Daftar'}
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
-        </Card>
+                            <Button
+                                type="submit"
+                                disabled={isSubmitting}
+                                className="btn btn-info mt-2 wide-button"
+                            >
+                                {isSubmitting ? 'Loading...' : 'Daftar'}
+                            </Button>
+                            <p className='mt-3 mb-5'>
+                                Sudah Punya Akun? <a href="#" className="link-dark"><b>Masuk</b></a>
+                            </p>
+                        </Form>
+                    </Col>
+                </Row>
+            </Card>
+        </Container>
     );
 }
